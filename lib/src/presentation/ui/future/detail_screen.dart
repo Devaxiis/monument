23c5543a9585cf6a01e4b98bab4703866f2f25f2
devaxis2303 <promer2303@gmail.com>
2024-10-26 21:28:22 +0500

    feat:(map route)=> Goolge mapga o'tib route chizadigan boldi.

--- lib/src/presentation/ui/future/detail_screen.dart
+++ lib/src/presentation/ui/future/detail_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:geocoding/geocoding.dart';
+import 'package:flutter/material.dart';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:monumentuz/library.dart';
+import 'package:monumentuz/src/domain/models/remote_models.dart/all_monument_list_data_model.dart';
 import 'package:monumentuz/src/presentation/ui/future/google_map_screen.dart';
 import 'package:monumentuz/src/presentation/widget/outlined_back_widget.dart';
 import 'package:monumentuz/src/utils/custom_widget/ui_toast.dart';
@@ -17,23 +20,23 @@ class DetailScreen extends StatefulWidget {
 
 class _DetailScreenState extends State<DetailScreen> {
   final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
+      Completer<GoogleMapController>();
 
 
-   final CameraPosition _kGooglePlex = const CameraPosition(
+  final CameraPosition _kGooglePlex = const CameraPosition(
     target: LatLng(41.311081, 69.240562),
     zoom: 14.4746,
   );
 
-   final CameraPosition _kLake = const CameraPosition(
+  final CameraPosition _kLake = const CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(41.311081, 69.240562),
       tilt: 59.440717697143555,
       zoom: 14.151926040649414);
 
+ 
 
-
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,26 +49,41 @@ class _DetailScreenState extends State<DetailScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                     padding: const EdgeInsets.symmetric(horizontal: 20),
-                     child: Row(
-                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                       children: [
-                        OutlinedBackWidget(icon: Icons.arrow_back,onTab: (){
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      OutlinedBackWidget(
+                        icon: Icons.arrow_back,
+                        onTab: () {
                           Navigator.of(context).pop();
-                        },),
-                        OutlinedBackWidget(icon: Icons.location_on_outlined,onTab: (){
-                          if(widget.item!.lon == '' &&widget.item!.lat == ''){
-                            UiToast.uiToastBottom("Manzil mavjud emas", context);
-                          }else{
-                          Navigator.of(context).push(MaterialPageRoute(builder: (_)=> MapSample(
-                            long:widget.item!.lon == ''?69.240562:double.parse(widget.item!.lon) ,
-                            lat:widget.item!.lon == ''?41.311081:double.parse(widget.item!.lat),),
-                          ));
+                        },
+                      ),
+                      OutlinedBackWidget(
+                        icon: Icons.location_on_outlined,
+                        onTab: () {
+                          if (widget.item!.lon == '' &&
+                              widget.item!.lat == '') {
+                            UiToast.uiToastBottom(
+                                "Manzil mavjud emas", context);
+                          } else {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (_) => MapSample(
+                               
+                                long: widget.item!.lon == ''
+                                    ? 69.240562
+                                    : double.parse(widget.item!.lon),
+                                lat: widget.item!.lon == ''
+                                    ? 41.311081
+                                    : double.parse(widget.item!.lat),
+                              ),
+                            ));
                           }
-                        },)
-                       ],
-                     ),
-                   ),
+                        },
+                      )
+                    ],
+                  ),
+                ),
                 const SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -74,12 +92,14 @@ class _DetailScreenState extends State<DetailScreen> {
                     width: MediaQuery.sizeOf(context).width,
                     margin: const EdgeInsets.symmetric(horizontal: 10),
                     decoration: BoxDecoration(
-                        image: DecorationImage(image:NetworkImage("${Apis.baseImgUrl}${widget.item!.attachments[0].contentURL}"),fit: BoxFit.cover),
-                        borderRadius: const BorderRadius.all(Radius.circular(15)),
-                        border: Border.all(
-                            width: 2,
-                            color: AppColors.color045646
-                        ),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "${Apis.baseImgUrl}${widget.item!.attachments[0].contentURL}"),
+                            fit: BoxFit.cover),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(15)),
+                        border:
+                            Border.all(width: 2, color: AppColors.color045646),
                         boxShadow: [
                           BoxShadow(
                             offset: const Offset(-5, 5),
@@ -87,26 +107,42 @@ class _DetailScreenState extends State<DetailScreen> {
                             blurRadius: 10,
                             spreadRadius: 3,
                           )
-                        ]
-                    ),
+                        ]),
                   ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child:context.locale == const Locale("en","EN")?
-                  Text(widget.item!.nameEng ,style: StyleUtil.detailNameStyle,):
-                  Text(widget.item!.nameUz ,style: StyleUtil.detailNameStyle,),
+                  child: context.locale == const Locale("en", "EN")
+                      ? Text(
+                          widget.item!.nameEng,
+                          style: StyleUtil.detailNameStyle,
+                        )
+                      : Text(
+                          widget.item!.nameUz,
+                          style: StyleUtil.detailNameStyle,
+                        ),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Text(widget.item!.buildAt,style: StyleUtil.detailDateStyle,),
+                  child: Text(
+                    widget.item!.buildAt,
+                    style: StyleUtil.detailDateStyle,
+                  ),
                 ),
                 const SizedBox(height: 20),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child:context.locale == const Locale("en","EN")?
-                  Text(Bidi.enforceLtrInText(widget.item!.descriptionEng),style: StyleUtil.detailDescStyle,textAlign: TextAlign.center,
-                  ):Text(Bidi.enforceLtrInText(widget.item!.descriptionUz),style: StyleUtil.detailDescStyle,textAlign: TextAlign.center,),
+                  child: context.locale == const Locale("en", "EN")
+                      ? Text(
+                          Bidi.enforceLtrInText(widget.item!.descriptionEng),
+                          style: StyleUtil.detailDescStyle,
+                          textAlign: TextAlign.center,
+                        )
+                      : Text(
+                          Bidi.enforceLtrInText(widget.item!.descriptionUz),
+                          style: StyleUtil.detailDescStyle,
+                          textAlign: TextAlign.center,
+                        ),
                 ),
               ],
             ),
@@ -115,6 +151,4 @@ class _DetailScreenState extends State<DetailScreen> {
       ),
     );
   }
-
-
 }
--- lib/src/presentation/ui/future/google_map_screen.dart
+++ lib/src/presentation/ui/future/google_map_screen.dart
@@ -1,18 +1,23 @@
 import 'dart:async';
+import 'dart:developer';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../../../library.dart';
 
 class MapSample extends StatefulWidget {
   final double lat;
   final double long;
-  const MapSample({super.key, required this.lat, required this.long});
+
+  const MapSample({super.key, required this.lat, required this.long,});
 
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
 class MapSampleState extends State<MapSample> {
-  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(41.311081, 69.240562),
@@ -22,10 +27,83 @@ class MapSampleState extends State<MapSample> {
   List dataLoc = [];
   bool isLoading = false;
 
+  LocationData? _currentLocation;
+  late Location location;
+
+  @override
+  void initState() {
+    super.initState();
+    location = Location();
+    _getLocation();
+   
+  }
+
+  Future<void> _getLocation() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    // Xizmatning yoqilganligini tekshirish
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    // Ruxsat olish
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    // Hozirgi joylashuvni olish
+    LocationData _locationData = await location.getLocation();
+
+    setState(() {
+      _currentLocation = _locationData;
+    });
+    setState(() {
+      
+    });
+  }
+ 
+
+  _openGoogleMaps() async {
+    if (_currentLocation == null) {
+      log("Location not available");
+      return;
+    }
+
+    double latitude = _currentLocation!.latitude!;
+    double longitude = _currentLocation!.longitude!;
+    String destination =
+        "${widget.lat},${widget.long}"; // Example destination (Tashkent coordinates)
+
+    Uri googleMapsUrl = Uri.parse("https://www.google.com/maps/dir/?api=1&origin=$latitude,$longitude&destination=$destination&travelmode=driving");
+
+      
+    if (await canLaunchUrl(googleMapsUrl)) {
+      await launchUrl(googleMapsUrl);
+    } else {
+      throw 'Could not open the map.';
+    }
+    setState((){});
+    setState((){});
+    setState((){});
+    setState((){});
+    setState((){});
+    setState((){});
+  
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:GoogleMap(
+      body: GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
@@ -34,26 +112,26 @@ class MapSampleState extends State<MapSample> {
         markers: Set<Marker>.of({
           Marker(
               markerId: MarkerId(UniqueKey().toString()),
-              position: LatLng(widget.lat,widget.long),
+              position: LatLng(widget.lat, widget.long),
               draggable: true,
-              onDragEnd: (end){}
-          ),
+              onDragEnd: (end) {}),
         }),
         zoomControlsEnabled: false,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _goToTheLake,
+        onPressed: _openGoogleMaps,
         backgroundColor: AppColors.color045646,
-        child: const Icon(Icons.zoom_in,color: AppColors.colorFFFFFF,),
+        child: const Icon(
+          Icons.directions,
+          color: AppColors.colorFFFFFF,
+        ),
       ),
     );
   }
 
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
-    await controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-        target: LatLng(widget.lat,widget.long),
-        zoom: 12.500
-    )));
+    await controller.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(widget.lat, widget.long), zoom: 12.500)));
   }
 }
